5b6ea88256a1781149cd2db46f43d8205e7da5d4
kav3 <kav3.com@gmail.com>
2025-04-19 19:51:03 -0700

    chore: change cache maxAge in code loader from github

--- server/api/v1/how-to-use/.get.ts
+++ server/api/v1/how-to-use/.get.ts
@@ -1,4 +1,4 @@
 export default defineCachedEventHandler((event) => {
     const { type, file } = getQuery(event)
     return fetch(!file ? `https://api.github.com/repos/noonjs/how-to-use/contents/${type}` : `https://raw.githubusercontent.com/noonjs/how-to-use/main/${type}/${file}`).then(x => x.text())
-}, { maxAge: 60 * 10 })
+}, { maxAge: 60 * 60 })
